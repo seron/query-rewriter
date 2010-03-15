8441b3702326d52882c47a26f1688430473afae4
Ivan Todorović <mirimiri66@gmail.com>
2010-03-15 07:49:41 +0100

    Initial commit

---
+++ src/qr/QueryRewriter.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package qr;
+
+/**
+ * @author ivan
+ *
+ */
+public class QueryRewriter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		// TODO blabla
+	}
+
+}
